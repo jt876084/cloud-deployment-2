87a7d2bd4d1835f5374632493c63ecb6921b6710
jt876084 <43050562+jt876084@users.noreply.github.com>
2018-09-17 19:54:10 -0400

    Update profile.py
    Removed silly.sh

--- profile.py
+++ profile.py
@@ -25,10 +25,5 @@ node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 
 node.routable_control_ip = True
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
-
-
-
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
